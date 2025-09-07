8c22ad139b7e57a6d7f372051c31ee923d01c6dd
oussama el hani <oussamaelhani3@gmail.com>
2025-09-07 19:29:32 +0100

    Ajout programme plan de santé selon âge et historique médical

---
+++ Day1_Conditions2_Challenge_8_Plan de Santé/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float sbase, sfinal, prime, thoraire, hsup;
+    int tposte;
+    printf("Entrez le salaire de base en euros : ");
+    scanf("%f", &sbase);
+    printf("Entrez le nombre d'heures supplementaires : ");
+    scanf("%f", &hsup);
+    printf("Type de poste (1-Junior, 2-Senior) : ");
+    scanf("%d", &tposte);
+    thoraire = sbase / 160.0;
+    if(tposte == 1) {
+        prime = sbase * 0.10;
+    } else {
+        prime = sbase * 0.20;
+    }
+    sfinal = sbase + (hsup * 1.5 * thoraire) + prime;
+    printf("Le salaire final est : %f euros\n", sfinal);
+    return 0;
+
+
+
+
+}
